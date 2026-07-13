a6130522a273fd88d026362e8b755433f6a8eda0
SSSAA-B <126969455+SSSAA-B@users.noreply.github.com>
2026-07-13 18:51:24 +0530

    Update Application.java

--- Application.java
+++ Application.java
@@ -7,3 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         // Starts the internal web server
+SpringApplication.run
+    (Application.class, args);
+    }
+}
